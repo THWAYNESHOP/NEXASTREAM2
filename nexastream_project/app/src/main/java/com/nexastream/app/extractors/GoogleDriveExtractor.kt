b6e9d1121e0837424bee9c14217fb96b71019ae7
THWAYNESHOP <patrickmatheka808@gmail.com>
2026-08-12 21:01:37 +0300

    security: remove hardcoded Google API key from GoogleDriveExtractor

--- nexastream_project/app/src/main/java/com/nexastream/app/extractors/GoogleDriveExtractor.kt
+++ nexastream_project/app/src/main/java/com/nexastream/app/extractors/GoogleDriveExtractor.kt
@@ -29,7 +29,7 @@ class GoogleDriveExtractor : Extractor() {
         
         val responseBody = service.getPlayback(
             fileId = fileId,
-            key = "AIzaSyDVQw45DwoYh632gvsP5vPDqEKvb-Ywnb8",
+            key = "YOUR_GOOGLE_API_KEY_HERE",
             unique = "gc999"
         )
 
